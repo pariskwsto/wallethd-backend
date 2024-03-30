b2881f5e433e4276b73376b51cde71a09d987131
Paris Kostopoulos <pariskwsto@gmail.com>
2024-03-30 22:52:13 +0200

    feat(subcategories): add seed data for subcategories

--- _data/seeder.js
+++ _data/seeder.js
@@ -10,6 +10,7 @@ const mongoose = require("mongoose");
 
 // load models
 const Category = require("../src/models/Category");
+const Subcategory = require("../src/models/Subcategory");
 
 // connect to DB
 mongoose.connect(process.env.MONGO_URI);
@@ -19,10 +20,15 @@ const categories = JSON.parse(
   fs.readFileSync(`${__dirname}/categories.json`, "utf-8")
 );
 
+const subcategories = JSON.parse(
+  fs.readFileSync(`${__dirname}/subcategories.json`, "utf-8")
+);
+
 // import into DB
 const importData = async () => {
   try {
     await Category.create(categories);
+    await Subcategory.create(subcategories);
 
     console.log("Data Imported...".green.inverse);
     process.exit();
@@ -35,6 +41,7 @@ const importData = async () => {
 const deleteData = async () => {
   try {
     await Category.deleteMany();
+    await Subcategory.deleteMany();
 
     console.log("Data Destroyed...".red.inverse);
     process.exit();
