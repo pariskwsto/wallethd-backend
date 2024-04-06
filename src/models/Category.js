1620c898e9c9933272119d7a4c290dfb4067b4fb
Paris Kostopoulos <pariskwsto@gmail.com>
2024-04-06 16:01:42 +0300

    feat(categories): populate subcategories in getAllCategories

--- src/models/Category.js
+++ src/models/Category.js
@@ -1,37 +1,60 @@
 const mongoose = require("mongoose");
+const slugify = require("slugify");
 
-const CategorySchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, "Please add a category name"],
-    maxlength: [60, "Category name can not be more than 60 characters"],
-    unique: true,
-    trim: true,
+const CategorySchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please add a category name"],
+      maxlength: [50, "Category name can not be more than 50 characters"],
+      unique: true,
+      trim: true,
+    },
+    slug: String,
+    description: {
+      type: String,
+      maxlength: [
+        250,
+        "Category description can not be more than 250 characters",
+      ],
+    },
+    icon: String,
+    order: {
+      type: Number,
+      default: 0,
+    },
+    transactionType: {
+      type: String,
+      required: [true, "Please add a transaction type"],
+      enum: ["income", "expense"],
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+    },
+    updatedAt: {
+      type: Date,
+      default: Date.now,
+    },
   },
-  description: {
-    type: String,
-    maxlength: [
-      250,
-      "Category description can not be more than 250 characters",
-    ],
-  },
-  order: {
-    type: Number,
-    default: 0,
-  },
-  transactionType: {
-    type: String,
-    required: [true, "Please add a transaction type"],
-    enum: ["income", "expense"],
-  },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-  },
-  updatedAt: {
-    type: Date,
-    default: Date.now,
-  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
+
+// create category slug from the name
+CategorySchema.pre("save", function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// reverse populate with virtuals
+CategorySchema.virtual("subcategories", {
+  ref: "Subcategory",
+  localField: "_id",
+  foreignField: "category",
+  justOne: false,
 });
 
 module.exports = mongoose.model("Category", CategorySchema, "categories");
