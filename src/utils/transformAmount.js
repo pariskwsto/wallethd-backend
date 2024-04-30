cb32e43add8ff3f71baed917dc7be92bf5d03efb
Paris Kostopoulos <pariskwsto@gmail.com>
2024-04-30 22:37:31 +0300

    refactor(utils): change transformAmount to named export

--- src/utils/transformAmount.js
+++ src/utils/transformAmount.js
@@ -1,9 +1,11 @@
 const Decimal = require("decimal.js");
 
-exports.transformAmountToInteger = (amount) => {
+const transformAmountToInteger = (amount) => {
   return parseFloat((amount * 100).toFixed(2));
 };
 
-exports.transformAmountToFloat = (amount) => {
+const transformAmountToFloat = (amount) => {
   return new Decimal(amount).dividedBy(100).toFixed(2);
 };
+
+module.exports = { transformAmountToInteger, transformAmountToFloat };
