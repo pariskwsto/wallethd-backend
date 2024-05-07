4a8d44041d4be00f6d4bd74352ebf49ace32e36f
Paris Kostopoulos <pariskwsto@gmail.com>
2024-05-07 23:02:42 +0300

    refactor(*): rename PORT env variable

--- src/config/express.js
+++ src/config/express.js
@@ -1,4 +1,4 @@
-const port = process.env.PORT || "5000";
+const port = process.env.SERVER_PORT || "5000";
 
 const publicUploadsBasePath =
   process.env.PUBLIC_UPLOADS_BASE_PATH || "./public/uploads";
